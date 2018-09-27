ee3ca68e2e3b531341a8a54a276aee64fe1d9581
Will Richards <richardsw2017@gmail.com>
2018-09-27 15:39:05 -0600

    structuring project and compiling with Cmake

---
+++ tools/verify.cpp
@@ -0,0 +1,33 @@
+#include <stdint.h>
+#include <iostream>
+#include <stdexcept>
+#include <stdio.h>
+#include <string>
+#include <vector>
+
+struct region{
+  std::string startChrom;
+  int32_t startPos;
+  std::string endChrom;
+  int32_t endPos;
+};
+
+int main(int argc, char *argv[]){
+
+    std::string regionString = argv[1];
+    
+    int16_t pos = regionString.find_first_of('-');
+    std::string startR = regionString.substr(0, pos);
+    std::string endR = regionString.substr(pos+1);
+    
+    int16_t startP = startR.find_first_of(':');
+    int16_t endP = endR.find_first_of(':');
+    
+    
+    region reg{.startChrom = startR.substr(0,startP), .startPos = std::stoi(startR.substr(startP+1)), .endChrom = endR.substr(0,endP), .endPos = std::stoi(endR.substr(endP+1))};
+    
+    
+    std::cout << "region is: " << reg.startChrom << ':' << reg.startPos << '-' << reg.endChrom << ':' << reg.endPos << std::endl;
+    
+    return 0;   
+}
