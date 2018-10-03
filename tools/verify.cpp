3a540c5d0852929d503cb8a1a477ca8f3f6e0ebe
Will Richards <richardsw2017@gmail.com>
2018-10-03 15:19:35 -0600

    parsing in Jhash files to kmer sets

--- tools/verify.cpp
+++ tools/verify.cpp
@@ -16,16 +16,50 @@ struct region{
   int32_t endPos;
 };
 
+struct proband{
+  std::vector<BamTools::BamAlignment> reads;
+};
 
 void bamToFasta(std::string bamFile){
   std::string command = "samtools bam2fq ";
   command += bamFile;
-  command += " | seqtk seq -A - > ";
+  command += " | seqtk seq -A - > temp/";
   command += util::baseName(bamFile);
   command += ".fa";
 
   std::cout << "running command " << command << std::endl;
   util::exec(command.c_str());
+}
+
+void runJelly(std::string fastaFile, int32_t kmerSize, int32_t threads){
+  std::string jellyfishPath = "/uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/bin/externals/jellyfish/src/jellyfish_project/bin/jellyfish";
+  std::string command = "";
+  command += jellyfishPath;
+  command += " count -m ";
+  command += std::to_string(kmerSize);
+  command += " -s 100M -t ";
+  command += std::to_string(threads);
+  command += " -o /uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.jf";
+  command += " -C ";
+  command += fastaFile;
+
+  std::cout << "executing command " << command << std::endl;
+  util::exec(command.c_str());
+
+  std::string histoCommand = "";
+  histoCommand += jellyfishPath;
+  histoCommand += " histo /uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.jf";
+
+  std::cout << "executing histo command " << histoCommand << std::endl;
+  util::exec(histoCommand.c_str());
+  
+  std::string dumpCommand = "";
+  dumpCommand += jellyfishPath;
+  dumpCommand += " dump /uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.jf > /uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.Jhash";
+  
+  std::cout << "executing dump command " << dumpCommand << std::endl;
+  util::exec(dumpCommand.c_str());
+  
 }
 
 //TODO: implement indexing bam
@@ -62,6 +96,33 @@ void writeBamRegion(region reg, std::string bamPath){
   if(!reader.SetRegion(region)){
     std::cout << "could not set region for coords: " << startRefID << ", " << reg.startPos << ", " << endRefID << ", " << reg.endPos << std::endl;
   }
+
+  proband p;
+
+  while(reader.GetNextAlignment(al)){
+    p.reads.push_back(al);
+    std::cout << "found read in region" << std::endl;
+  }
+
+}
+
+//std::vector<std::map<std::string, int32_t> > jhashToMap(std::string jhashPath){
+void jhashToMap(std::string jhashPath){
+  std::ifstream file(jhashPath);
+  std::string line;
+  
+  std::ifstream newfile(jhashPath);
+  while(std::getline(newfile, line)){
+    std::cout << "first line is: " << line << std::endl;
+    std::getline(newfile, line);
+    std::cout << "second line is: " << line << std::endl;
+  }
+}
+
+
+//std::vector<std::map<std::string, int32_t> > getUniqueHashes(std::string probandUniqueHashList, std::string regionJhash){
+void getUniqueHashes(std::string probandUniqueHashList, std::string regionJhash){
+  jhashToMap(regionJhash);
 }
 
 
@@ -84,8 +145,10 @@ int main(int argc, char *argv[]){
     std::cout << "subject bam file is: " << subjectBam << std::endl;
 
     bamToFasta(subjectBam);
-    writeBamRegion(reg, subjectBam);
-    
+    //writeBamRegion(reg, subjectBam);
+    std::string fasta = "/uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/Child.bam.fa";
+    runJelly(fasta, 25, 10);
+    getUniqueHashes("/uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.Jhash", "/uufs/chpc.utah.edu/common/home/u0401321/mutationVerifier/temp/fasta.Jhash");
     return 0;   
 }
 
